095a0552baf83748b6109895ccba2416b6fbe23e
flint <olusegun.sola19@gmail.com>
2020-03-02 12:42:52 +0100

    production code in server

--- server.js
+++ server.js
@@ -24,6 +24,15 @@ mongoose
 .catch(err => console.log(err));
 
 
+if(process.env.NODE_ENV === 'production') {
+    // app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) => {
+      // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+      res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+    })
+  }
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
