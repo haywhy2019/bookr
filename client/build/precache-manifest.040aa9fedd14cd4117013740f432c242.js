80dd9a1b064146c4ad6faf3aba5b56bec6950768
flint <olusegun.sola19@gmail.com>
2020-03-02 12:25:39 +0100

    build

---
+++ client/build/precache-manifest.040aa9fedd14cd4117013740f432c242.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495a5d677ba16c5d6a70abf4afe6f665",
+    "url": "/bookr-app/index.html"
+  },
+  {
+    "revision": "00daacad909990252cdf",
+    "url": "/bookr-app/static/css/2.d90538a7.chunk.css"
+  },
+  {
+    "revision": "c7c4e2b2af4c4606b44b",
+    "url": "/bookr-app/static/css/main.ef7c2978.chunk.css"
+  },
+  {
+    "revision": "00daacad909990252cdf",
+    "url": "/bookr-app/static/js/2.d4c5ab56.chunk.js"
+  },
+  {
+    "revision": "4c3b865ea8d319faf1c56c47d6e1060e",
+    "url": "/bookr-app/static/js/2.d4c5ab56.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7c4e2b2af4c4606b44b",
+    "url": "/bookr-app/static/js/main.72353ad7.chunk.js"
+  },
+  {
+    "revision": "35d20c8319b3d431a4e8",
+    "url": "/bookr-app/static/js/runtime-main.cdc0c9c1.js"
+  },
+  {
+    "revision": "6e81ea2d39b4a28544430137f72add02",
+    "url": "/bookr-app/static/media/Boardroom.6e81ea2d.jpeg"
+  },
+  {
+    "revision": "939625fc4dfa8ab0e9f63da010c6bd34",
+    "url": "/bookr-app/static/media/Hero.939625fc.jpeg"
+  },
+  {
+    "revision": "c6d1a9e56a03ab73111099c946593541",
+    "url": "/bookr-app/static/media/Marketingroom.c6d1a9e5.jpeg"
+  },
+  {
+    "revision": "50d7a06ddd78077b10852c61779ea497",
+    "url": "/bookr-app/static/media/Salesroom.50d7a06d.jpeg"
+  },
+  {
+    "revision": "27d8105fea8f4be9482d3a03e8c7917e",
+    "url": "/bookr-app/static/media/Techroom.27d8105f.jpeg"
+  },
+  {
+    "revision": "c8e40c429a19b298f767ef419a97a079",
+    "url": "/bookr-app/static/media/aryastark.c8e40c42.jpeg"
+  },
+  {
+    "revision": "af1a238029e9d5bd86e89d2742eb740d",
+    "url": "/bookr-app/static/media/cerseilannister.af1a2380.jpeg"
+  },
+  {
+    "revision": "4032fe0c43b1dede2e462ab1edc9898a",
+    "url": "/bookr-app/static/media/jonsnow.4032fe0c.jpeg"
+  }
+]);
