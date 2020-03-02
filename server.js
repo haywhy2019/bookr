4b4b7924fe959cfa453ea8f614fe35667e22bb5b
flint <olusegun.sola19@gmail.com>
2020-03-02 23:21:45 +0100

    make it better

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const bodyParser = require('body-parser');
 const path = require('path')
 const cors = require('cors')
 
+//added
+const {createServer} = require ('http');
+const compression = require('compression');
+const morgan = require ('morgan');
+const normalizePort = port => parseInt(port, 10);
+
 
 const app = express();
 
@@ -25,15 +31,39 @@ mongoose
 .catch(err => console.log(err));
 
 
-if(process.env.NODE_ENV === 'production') {
-    // app.use(express.static('client/build'));
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => {
-      // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-      res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
-    })
-  }
+// if(process.env.NODE_ENV === 'production') {
+//     // app.use(express.static('client/build'));
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+//     app.get('*', (req, res) => {
+//       // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//       res.sendFile(path.join(__dirname, '/client/build', 'index.html'))
+//     })
+//   }
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
+//new
+const PORT = normalizePort(process.env.PORT || 5000);
+const dev = app.get('env') !=+ 'production';
+
+if(!dev){
+  app.disable('x-powered-by');
+  app.use(compression());
+  app.use(morgan('common'));
+
+  app.use(express.static(path.join(__dirname,'build')))
+
+  app.get('/',(req,res)=>{
+      res.sendFile(path.join(__dirname,'build','index.html'))
+  })
+}
+
+if(dev){
+  app.use(morgan('dev'))
+  app.disable('x-powered-by');
+  app.use(compression());
+  app.use(morgan('common'));
+
+  app.use(express.static(path.join(__dirname,'build')))
+
 
 app.listen(port, () => console.log(`server started on port ${port}`));
